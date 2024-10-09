0bd30fc129816102312106e03f5db56c4a97a32c
elijahelephant <elijahwilliams@ucsb.edu>
2024-10-09 16:09:28 -0700

    updated name stuff

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -20,7 +20,7 @@ public class Developer {
         // You may use just the name that is used on <https://bit.ly/cs156-f24-teams>
         // i.e. your first name, or your first and initial of last name
 
-        return "Chris G.";
+        return "Elijah";
     }
 
     /**
@@ -30,7 +30,7 @@ public class Developer {
 
     public static String getGithubId() {
         // TODO: Change this to your github id
-        return "cgaucho";
+        return "elijahelephant";
     }
 
     /**
@@ -40,7 +40,7 @@ public class Developer {
     
     public static Team getTeam() {
         // TODO: Change this to your team name
-        Team team = new Team("f24-xx");
+        Team team = new Team("f24-09");
         team.addMember("Alice");
         team.addMember("Bob");
         team.addMember("Chris G.");
